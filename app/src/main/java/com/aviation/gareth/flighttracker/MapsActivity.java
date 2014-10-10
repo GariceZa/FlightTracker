a262137f2a5cdc096aad055aa07b7263cd65edb2
GariceZa <garethpriceza@gmail.com>
2014-10-10 18:38:01 +0200

    added to git

--- app/src/main/java/com/aviation/gareth/flighttracker/MapsActivity.java
+++ app/src/main/java/com/aviation/gareth/flighttracker/MapsActivity.java
@@ -64,6 +64,7 @@ public class MapsActivity extends FragmentActivity implements  android.location.
     public boolean onCreateOptionsMenu(Menu menu) {
     //Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
